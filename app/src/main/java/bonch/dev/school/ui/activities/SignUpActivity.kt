30479045e22c56eff82a04b704fac33bbfd2665c
VeronikaSkornyakova <veronskor@gmail.com>
2019-10-18 17:00:38 +0300

    Start!

--- app/src/main/java/bonch/dev/school/ui/activities/SignUpActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/SignUpActivity.kt
@@ -2,6 +2,7 @@ package bonch.dev.school.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import bonch.dev.school.R
 
 class SignUpActivity : AppCompatActivity() {
 
--- app/src/main/java/bonch/dev/school/ui/activities/SignlnActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/SignlnActivity.kt
@@ -2,11 +2,12 @@ package bonch.dev.school.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import bonch.dev.school.R
 
 class SignlnActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super.onCreate (savedInstanceState)
         setContentView(R.layout.activity_signln)
     }
 }
